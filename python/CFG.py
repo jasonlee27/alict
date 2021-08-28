b988d7d4bc895e326f8df08724a220bd16eb6031
jasonlee27 <leej941@gmail.com>
2021-08-28 17:07:30 -0500

    write generated cfg to file

--- python/CFG.py
+++ python/CFG.py
@@ -8,7 +8,7 @@ import spacy
 from nltk.corpus import treebank
 
 class CFG:
-    benepar_parser_model = "benepar_en3"
+    benepar_parser_model = 'benepar_en3'
     
     @classmethod
     def load_parser(cls):
@@ -16,7 +16,7 @@ class CFG:
         if spacy.__version__.startswith('2'):
             nlp.add_pipe(benepar.BeneparComponent(cls.benepar_parser_model))
         else:
-            nlp.add_pipe("benepar", config={"model": cls.benepar_parser_model})
+            nlp.add_pipe('benepar', config={'model': cls.benepar_parser_model})
         #end if
         return nlp
 
@@ -37,14 +37,14 @@ class CFG:
     def get_cfg_per_tree(cls, tree, rule_dict):
         left = tree._.labels
         if len(left)==0: # if terminal
-            re_search = re.search("\(([A-Z]+)\s(.+)\)", tree._.parse_string)
+            re_search = re.search('\(([A-Z]+)\s(.+)\)', tree._.parse_string)
             rlabel = re_search.group(1)
             rword = re_search.group(2)
             plabel = tree._.parent._.labels[0]
             if rlabel not in rule_dict.keys():
-                rule_dict[rlabel] = [f"terminal::{rword}"]
+                rule_dict[rlabel] = [f'terminal::{rword}']
             elif (rlabel, rword) not in rule_dict[rlabel]:
-                rule_dict[rlabel].append(f"terminal::{rword}")
+                rule_dict[rlabel].append(f'terminal::{rword}')
             # end if
         else:
             llabel = left[0]
@@ -58,7 +58,7 @@ class CFG:
                     if len(list(r._.labels))>0:
                         non_terminals.append(r._.labels[0])
                     else:
-                        re_search = re.search("\(([A-Z]+)\s(.+)\)", r._.parse_string)
+                        re_search = re.search('\(([A-Z]+)\s(.+)\)', r._.parse_string)
                         rlabel = re_search.group(1)
                         rword = re_search.group(2)
                         non_terminals.append(rlabel)
@@ -69,43 +69,76 @@ class CFG:
                     rule_dict[llabel].append(tuple(non_terminals))
                 # end if
             else:
-                re_search = re.search(f"\({llabel}\s\(([A-Z]+)\s(.+)\)\)$", tree._.parse_string)
+                re_search = re.search(f'\({llabel}\s\(([A-Z]+)\s(.+)\)\)$', tree._.parse_string)
                 rlabel = re_search.group(1)
                 rword = re_search.group(2)
                 if (rlabel) not in rule_dict[llabel]:
                     rule_dict[llabel].append((rlabel))
                 # end if
                 if rlabel not in rule_dict.keys():
-                    rule_dict[rlabel] = [f"terminal::{rword}"]
-                elif f"terminal::{rword}" not in rule_dict[rlabel]:
-                    rule_dict[rlabel].append(f"terminal::{rword}")
+                    rule_dict[rlabel] = [f'terminal::{rword}']
+                elif f'terminal::{rword}' not in rule_dict[rlabel]:
+                    rule_dict[rlabel].append(f'terminal::{rword}')
                 # end if
             # end if
         # end if
         return rule_dict
 
     @classmethod
-    def get_cfg(cls, parser, sents, rule_dict):
+    def get_cfg_dict(cls, parser, sents, rule_dict):
         for s in sents:
             tree = CFG.get_tree(parser,s.strip())
             rule_dict = CFG.get_cfg_per_tree(tree, rule_dict)
-            print(rule_dict)
-            print()
+            # print(rule_dict)
+            # print()
         # end for
         return rule_dict
 
-    # @classmethod
-    # def main(cls):
-    #     rulesets = set(rule for tree in treebank.parsed_sents()[:10] for rule in tree.productions())
-    #     for r in rulesets:
-    #         print(r)
-    #         print()
-    #     # end for
+    @classmethod
+    def convert_cfg_dict_to_str(cls, cfg_dict):
+        cfg_str = ''
+        for left, rights in cfg_dict.items():
+            cfg_elem = f'{left} -> '
+            for r_i, right in enumerate(rights):
+                if type(right) is tuple:
+                    r_str = ' '.join(right)
+                    cfg_elem += f'{r_str}'
+                elif right.startswith('terminal::'):
+                    r_str = right.split('terminal::')[-1]
+                    cfg_elem += f'\'{r_str}\''
+                else:
+                    cfg_elem += right
+                # end if
+                if r_i+1<len(rights):
+                    cfg_elem += ' | '
+                # end if
+            # end for
+            cfg_str += f'{cfg_elem}\n'
+        # end for
+        return cfg_str
+    
+    @classmethod
+    def write_cfg(cls, cfg_str, cfg_file):
+        with open(cfg_file, 'w') as f:
+            f.write(cfg_str)
+        # end with
+
+
+    @classmethod
+    def main(cls):
+        rulesets = set(rule for tree in treebank.parsed_sents()[:10] for rule in tree.productions())
+        for r in rulesets:
+            print(r)
+            print()
+        # end for
 
 def main():
     parser = CFG.load_parser()
     sents: List = CFG.read_input_sents('./ex.txt')
-    tree = CFG.get_cfg(parser,sents,{})
+    cfg_dict = CFG.get_cfg_dict(parser,sents,{})
+    cfg_str = CFG.convert_cfg_dict_to_str(cfg_dict)
+    CFG.write_cfg(cfg_str, './ex_cfg.txt')
+    # CFG.main()
 
-if __name__=="__main__":
+if __name__=='__main__':
     main()
