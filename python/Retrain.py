65e5f7d4632d8154187fd4b8271b504d383c8a2a
Jaeseong Lee <jason.lee27@utexas.edu>
2021-11-26 03:05:44 -0600

    add retrain class

---
+++ python/Retrain.py
@@ -0,0 +1,42 @@
+# This script is to re-test models using failed cases
+# found from testsuite results
+
+from typing import *
+from pathlib import Path
+
+from checklist.test_suite import TestSuite as suite
+
+from Macros import Macros
+from Utils import Utils
+from Testsuite import Testsuite
+from Model import Model
+
+import os
+import sys
+
+class Retrain:
+
+    @classmethod
+    def get_failed_cases_from_test_results(cls):
+        pass
+
+    @classmethod
+    def load_model(cls):
+        pass
+
+    @classmethod
+    def train(cls):
+        pass
+
+    @classmethod
+    def evaluate(cls):
+        pass
+
+    @classmethod
+    def test(cls):
+        pass
+
+    
+
+    
+    
