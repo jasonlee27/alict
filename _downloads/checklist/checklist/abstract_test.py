02620efdf9d7c8e67683a9da908279839d0de3ff
jasonlee27 <leej941@gmail.com>
2022-08-17 15:08:13 -0500

    debugging

--- _downloads/checklist/checklist/abstract_test.py
+++ _downloads/checklist/checklist/abstract_test.py
@@ -491,12 +491,12 @@ class AbstractTest(ABC):
 
         if format_example_fn is None:
             format_example_fn = default_format_example
-            
+        
         passs = self.pass_idxs()
         if passs.shape[0] != 0:
             _print()
             _print('Example passes:')
-            passs = np.random.choice(passs, min(passs.shape[0], n), replace=False)
+            # passs = np.random.choice(passs, min(passs.shape[0], n), replace=False)
             for p in passs:
                 d_idx = p if self.run_idxs is None else self.run_idxs[p]
                 # should be format_fn
@@ -510,7 +510,7 @@ class AbstractTest(ABC):
             return
         _print()
         _print('Example fails:')
-        fails = np.random.choice(fails, min(fails.shape[0], n), replace=False)
+        # fails = np.random.choice(fails, min(fails.shape[0], n), replace=False)
         for f in fails:
             d_idx = f if self.run_idxs is None else self.run_idxs[f]
             # should be format_fn
