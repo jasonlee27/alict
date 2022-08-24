ccbeb23ff31f13570e08bd6ddb127b5f68c6ee8b
jasonlee27 <leej941@gmail.com>
2022-08-24 00:27:15 -0500

    debug cfg

--- python/sa/testsuite/cfg/CFGDiff.py
+++ python/sa/testsuite/cfg/CFGDiff.py
@@ -105,7 +105,8 @@ class CFGDiff:
                 # end for
                 return lhs, tuple(rhs), tuple(words)
             else:
-                search = re.search(f"\(([^\s\(]+)\s([^\s\)]+)\)", seed_tree._.parse_string)
+                # search = re.search(f"\(([^\s\(]+)\s([^\s\)]+)\)", seed_tree._.parse_string)
+                search = re.search(f"\(([^s\(]+)\s([^\(\)]+)\)", seed_tree._.parse_string)
                 rhs.append(search.group(1))
                 return lhs, tuple(rhs), str(seed_tree)
             # end if            
