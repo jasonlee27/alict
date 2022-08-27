ce452e9427df39be4b1e600afca1ab03c133a807
jasonlee27 <leej941@gmail.com>
2022-08-27 00:24:52 -0500

    pdr debug

--- python/sa/exp/ProductionruleCoverage.py
+++ python/sa/exp/ProductionruleCoverage.py
@@ -1,6 +1,7 @@
 import os
 import nltk
 import time
+import random
 import multiprocessing
 
 from tqdm import tqdm
@@ -97,63 +98,73 @@ class ProductionruleCoverage:
                                selection_method,
                                num_seeds,
                                num_trials,
+                               parse_all_sents=False,
                                logger=None):
-        if num_seeds<0:
-            cfg_res_file = Macros.result_dir / f"cfg_expanded_inputs{num_trials}_{task}_{dataset_name}_{selection_method}.json"
-            res_file = Macros.pdr_cov_result_dir / f"seed_cfg_rules{num_trials}_{task}_{dataset_name}.json"
-        else:
-            cfg_res_file = Macros.result_dir / f"cfg_expanded_inputs{num_trials}_{task}_{dataset_name}_{selection_method}_{num_seeds}seeds.json"
-            res_file = Macros.pdr_cov_result_dir / f"seed_cfg_rules{num_trials}_{task}_{dataset_name}.json"
-        # end if
-        if os.path.exists(cfg_res_file):
-            seed_rules = dict()
-            cfg_results = Utils.read_json(cfg_res_file)
-            for cfg_res in cfg_results:
-                lc = cfg_res['requirement']['description']
-                seed_inputs = list()
-                seed_rules[lc] = dict()
-                if logger is not None:
-                    logger.print(f"OUR_SEED::{lc}::")
+        if not parse_all_sents:
+            seed_rules_over_trials = list()
+            for t in range(int(num_trials)):
+                num_trial = '' if t==0 else str(t+1)
+                cfg_res_file = Macros.result_dir / f"cfg_expanded_inputs{num_trial}_{task}_{dataset_name}_{selection_method}_{num_seeds}seeds.json"
+                res_file = Macros.pdr_cov_result_dir / f"seed_pdr{num_trial}_{task}_{dataset_name}_{num_seeds}seeds.json"
+                cfg_results = Utils.read_json(cfg_res_file)
+                if os.path.exists(res_file):
+                    seed_rules = Utils.read_json(res_file)
+                else:
+                    seed_rules = dict()
                 # end if
+                for cfg_res in cfg_results:
+                    lc_desc = cfg_res['requirement']['description']
+                    if lc_desc not in seed_rules.keys():
+                        req_cksum = Utils.get_cksum(lc_desc)
+                        seed_inputs = list()
+                        seed_rules[lc_desc] = dict()
+                        if logger is not None:
+                            logger.print(f"OUR_SEED::{lc_desc}")
+                        # end if
                 
-                for seed in cfg_res['inputs'].keys():
-                    cfg_seed = cfg_res['inputs'][seed]['cfg_seed']
-                    seed_rules[lc][seed] = get_pdr_per_sent(cfg_seed)
+                        for seed in cfg_res['inputs'].keys():
+                            cfg_seed = cfg_res['inputs'][seed]['cfg_seed']
+                            seed_rules[lc_desc][seed] = get_pdr_per_sent(cfg_seed)
+                        # end for
+                        # Utils.write_json(seed_rules, res_file, pretty_format=True)
+                    # end if
                 # end for
-                Utils.write_json(seed_rules, res_file, pretty_format=True)
+                seed_rules_over_trials.append(seed_rules)
             # end for
+            return seed_rules_over_trials
         else:
-            if num_seeds<0:
-                data_file = Macros.result_dir / f"seed_inputs{num_trials}_{task}_{dataset_name}.json"
-                res_file = Macros.result_dir / f"seed_cfg_rules{num_trials}_{task}_{dataset_name}.json"
-            else:
-                data_file = Macros.result_dir / f"seed_inputs{num_trials}_{task}_{dataset_name}_{num_seeds}seeds.json"
-                res_file = Macros.result_dir / f"seed_cfg_rules{num_trials}_{task}_{dataset_name}_{num_seeds}seeds.json"
-            # end if
-            seed_dicts = Utils.read_json(data_file)
+            data_dir = Macros.result_dir / f"seeds_{task}_{dataset_name}"
+            res_file = Macros.pdr_cov_result_dir / f"seed_pdr_{task}_{dataset_name}.json"
+            cksum_vals = [
+                (l.strip().split('\t')[0], l.strip().split('\t')[1])
+                for l in Utils.read_txt(data_dir / 'cksum_map.txt')
+            ]
             if os.path.exists(res_file):
                 seed_rules = Utils.read_json(res_file)
             else:
                 seed_rules = dict()
             # end if
-            for seed in seed_dicts:
-                lc = seed['requirement']['description']
-                if lc not in seed_rules.keys():
-                    if logger is not None:
-                        logger.print(f"OUR_SEED::{lc}::")
+            for lc_desc, cksum_val in cksum_vals:
+                seed_dicts = Utils.read_json(data_dir / f"seed_{cksum_val}.json")
+                for seed in seed_dicts:
+                    if lc_desc not in seed_rules.keys():
+                        if logger is not None:
+                            logger.print(f"OUR_SEED::{lc_desc}::")
+                        # end if
+                        seed_rules[lc_desc] = dict()
+                        args = [(s[1],) for s in seed['seeds']]
+                        pool = Pool(processes=NUM_PROCESSES)
+                        results = pool.starmap_async(get_cfg_rules_per_sent,
+                                                     args,
+                                                     chunksize=len(args) // NUM_PROCESSES).get()
+                        for r in results:
+                            seed_rules[lc_desc][r['sent']] = r['cfg_rules']
+                        # end for
+                        Utils.write_json(seed_rules, res_file, pretty_format=True)
                     # end if
-                    seed_rules[lc] = dict()
-                    args = [(s[1],) for s in seed['seeds']]
-                    pool = Pool(processes=NUM_PROCESSES)
-                    results = pool.starmap_async(get_cfg_rules_per_sent,
-                                                 args,
-                                                 chunksize=len(args) // NUM_PROCESSES).get()
-                    for r in results:
-                        seed_rules[lc][r['sent']] = r['cfg_rules']
-                    # end for
-                    Utils.write_json(seed_rules, res_file, pretty_format=True)
-                # end if
+                # end for
             # end for
+            return seed_rules
         # end if
         
         # if num_seeds<0:
@@ -187,7 +198,7 @@ class ProductionruleCoverage:
         #         Utils.write_json(seed_rules, res_file, pretty_format=True)
         #     # end if
         # # end for
-        return seed_rules
+        return
 
     @classmethod
     def get_our_exp_cfg_rules(cls,
@@ -243,11 +254,12 @@ class ProductionruleCoverage:
                          num_seeds,
                          num_trials,
                          logger=None):
+        _num_trials = '' if num_trials==1 else str(num_trials)
         if num_seeds<0:
-            data_file = Macros.result_dir / f"cfg_expanded_inputs{num_trials}_{task}_{dataset_name}_{selection_method}.json"
+            data_file = Macros.result_dir / f"cfg_expanded_inputs{_num_trials}_{task}_{dataset_name}_{selection_method}.json"
             res_file = Macros.pdr_cov_result_dir / f"bl_cfg_rules_{task}_checklist.json"
         else:
-            data_file = Macros.result_dir / f"cfg_expanded_inputs{num_trials}_{task}_{dataset_name}_{selection_method}_{num_seeds}seeds.json"
+            data_file = Macros.result_dir / f"cfg_expanded_inputs{_num_trials}_{task}_{dataset_name}_{selection_method}_{num_seeds}seeds.json"
             res_file = Macros.pdr_cov_result_dir / f"bl_cfg_rules_{task}_checklist.json"
         # end if
         seed_dicts = Utils.read_json(data_file)
@@ -259,7 +271,6 @@ class ProductionruleCoverage:
         for seed in seed_dicts:
             lc = seed['requirement']['description']
             if lc not in bl_rules.keys():
-                print(lc)
                 if logger is not None:
                     logger.print(f"BL::{lc}::")
                 # end if
@@ -288,25 +299,26 @@ def main_seed(task,
               num_seeds,
               num_trials):
     st = time.time()
-    num_trials = '' if num_trials==1 else str(num_trials)
+    _num_trials = '' if num_trials==1 else str(num_trials)
     if num_seeds<0:
-        logger_file = Macros.log_dir / f"seeds{num_trials}_{task}_{search_dataset_name}_pdrcov.log"
-        result_file = Macros.pdr_cov_result_dir / f"seeds{num_trials}_{task}_{search_dataset_name}_pdrcov.json"
+        logger_file = Macros.log_dir / f"seeds_{task}_{search_dataset_name}_pdrcov.log"
+        result_file = Macros.pdr_cov_result_dir / f"seeds_{task}_{search_dataset_name}_pdrcov.json"
     else:
-        logger_file = Macros.log_dir / f"seeds{num_trials}_{task}_{search_dataset_name}_{num_seeds}seeds_pdrcov.log"
-        result_file = Macros.pdr_cov_result_dir / f"seeds{num_trials}_{task}_{search_dataset_name}_{num_seeds}seeds_pdrcov.json"
+        logger_file = Macros.log_dir / f"seeds{_num_trials}_{task}_{search_dataset_name}_{num_seeds}seeds_pdrcov.log"
+        result_file = Macros.pdr_cov_result_dir / f"seeds{_num_trials}_{task}_{search_dataset_name}_{num_seeds}seeds_pdrcov.json"
     # end if
     Macros.pdr_cov_result_dir.mkdir(parents=True, exist_ok=True)
     logger = Logger(logger_file=logger_file,
                     logger_name='seed_pdrcov_log')
-    # seed_rules = ProductionruleCoverage.get_our_seed_cfg_rules(
-    #     task,
-    #     search_dataset_name,
-    #     selection_method,
-    #     num_seeds,
-    #     num_trials,
-    #     logger=logger
-    # )
+    seed_rules = ProductionruleCoverage.get_our_seed_cfg_rules(
+        task,
+        search_dataset_name,
+        selection_method,
+        num_seeds,
+        num_trials,
+        parse_all_sents=False,
+        logger=logger
+    )
     checklist_rules = ProductionruleCoverage.get_bl_cfg_rules(
         task,
         search_dataset_name,
@@ -315,27 +327,96 @@ def main_seed(task,
         num_trials,
         logger=logger
     )
-    raise()
     if os.path.exists(str(result_file)):
         scores = Utils.read_json(result_file)
     else:
         scores = dict()
     # end if
-    
-    for lc in tqdm(seed_rules.keys()):
-        if lc not in scores.keys():
-            logger.print(f"OURS::{lc}", end='::')
-            pdr_obj = ProductionruleCoverage(lc=lc,
-                                             our_cfg_rules=seed_rules[lc],
-                                             bl_cfg_rules=checklist_rules[lc])
-            scores[lc] = {
-                'our_num_data': pdr_obj.our_num_data,
-                'bl_num_data': pdr_obj.bl_num_data,
-                'coverage_score': pdr_obj.get_score()
-            }
-            Utils.write_json(scores, result_file, pretty_format=True)
-        # end if
-    # end for
-    ft = time.time()
-    logger.print(f"ProductionruleCoverage.main_seed::{round(ft-st,3)}sec")
+
+    if type(seed_rules)==dict:
+        for lc in tqdm(seed_rules.keys()):
+            if lc not in scores.keys():
+                logger.print(f"OURS::{lc}", end='::')
+                pdr_obj = ProductionruleCoverage(lc=lc,
+                                                 our_cfg_rules=seed_rules[lc],
+                                                 bl_cfg_rules=checklist_rules[lc])
+                scores[lc] = {
+                    'our_num_data': pdr_obj.our_num_data,
+                    'bl_num_data': pdr_obj.bl_num_data,
+                    'coverage_score': pdr_obj.get_score()
+                }
+                Utils.write_json(scores, result_file, pretty_format=True)
+            # end if
+        # end for
+        ft = time.time()
+        logger.print(f"ProductionruleCoverage.main_seed::{round(ft-st,3)}sec")
+    else:
+        for lc in tqdm(checklist_rules.keys()):
+            if lc not in scores.keys():
+                scores[lc] = {
+                    'ours': {
+                        'coverage_scores': list()
+                    },
+                    'bl': {
+                        'coverage_scores': list()
+                    }
+                }
+                if logger is not None:
+                    logger.print(f"OURS::{lc}")
+                # end if
+                for num_trial in range(len(seed_rules)):
+                    random.seed(num_trial)
+                    seed_rules_per_trial = seed_rules[num_trial]
+                    _seed_rules = seed_rules_per_trial[lc]
+                    _bl_rules = checklist_rules[lc]
+                    bl_sents = list(_bl_rules.keys())
+                    seed_sents = list(_seed_rules.keys())
+                    # sample bl pdrs to make same number of pdrs with ours
+                    pdr1, pdr2 = dict(), dict()
+                    if len(seed_sents)<len(bl_sents):
+                        num_sample = len(seed_sents)
+                        r_idxs = list(range(num_sample))
+                        random.shuffle(r_idxs)
+                        bl_sents_sample = [bl_sents[r_i] for r_i in r_idxs[:num_sample]]
+                        pdr1 = {
+                            s: _seed_rules[s]
+                            for s in seed_sents
+                        }
+                        pdr2 = {
+                            s: _bl_rules[s]
+                            for s in bl_sents_sample
+                        }
+                    else:
+                        num_sample = len(seed_sents)
+                        r_idxs = list(range(num_sample))
+                        random.shuffle(r_idxs)
+                        seed_sents_sample = [seed_sents[r_i] for r_i in r_idxs[:num_sample]]
+                        pdr1 = {
+                            s: _seed_rules[s]
+                            for s in seed_sents_sample
+                        }
+                        pdr2 = {
+                            s: _bl_rules[s]
+                            for s in bl_sents
+                        }
+                    # end if
+                    pdr_obj = ProductionruleCoverage(lc=lc,
+                                                     our_cfg_rules=pdr1,
+                                                     bl_cfg_rules=pdr2)
+                    scores[lc]['num_data'] = pdr_obj.our_num_data
+                    cov_score_ours, cov_score_bl = pdr_obj.get_score()
+                    scores[lc]['ours']['coverage_scores'].append(cov_score_ours)
+                    scores[lc]['bl']['coverage_scores'].append(cov_score_bl)
+                # end for
+                print()
+                scores[lc]['ours']['avg_score'] = Utils.avg(scores[lc]['ours']['coverage_scores'])
+                scores[lc]['ours']['med_score'] = Utils.median(scores[lc]['ours']['coverage_scores'])
+                scores[lc]['ours']['std_score'] = Utils.stdev(scores[lc]['ours']['coverage_scores'])
+                scores[lc]['bl']['avg_score'] = Utils.avg(scores[lc]['bl']['coverage_scores'])
+                scores[lc]['bl']['med_score'] = Utils.median(scores[lc]['bl']['coverage_scores'])
+                scores[lc]['bl']['std_score'] = Utils.stdev(scores[lc]['bl']['coverage_scores'])
+                Utils.write_json(scores, result_file, pretty_format=True)
+            # end if
+        # end for
+    # end if
     return
