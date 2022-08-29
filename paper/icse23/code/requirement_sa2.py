108ddc7e7b1bdf2228522cbf32e5e1cf5d8cfdb2
jaeseong lee <jaeseonglee@jaeseongs-MacBook-Pro.local>
2022-08-29 15:21:36 -0500

    add icse

---
+++ paper/icse23/code/requirement_sa2.py
@@ -0,0 +1,11 @@
+SRL_PHASE_TEMPLATE = {
+    "prefix": [
+        "Some people think that",
+        "Many people agree with that",
+        "They think that",
+        "You agree with that"
+    ],
+    "sent1": [],
+    "middlefix": ["but I think that"],
+    "sent2": [],
+}
