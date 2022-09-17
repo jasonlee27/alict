6a77e5945ddeaf7417a9e960ab35b15f4a25a053
jasonlee27 <leej941@gmail.com>
2022-09-17 13:25:05 -0500

    hatecheck ph values

---
+++ python/hs/testsuite/hatecheck/Hatecheck.py
@@ -0,0 +1,38 @@
+# this script extracts protected group words
+# listed in hatecheck-data repo
+# Link: https://github.com/paul-rottger/hatecheck-data
+
+from typing import *
+
+import re, os
+import sys
+import json
+import random
+
+from pathlib import Path
+from ...utils.Macros import Macros
+from ...utils.Utils import Utils
+
+
+class Hatecheck:
+
+    hatecheck_placeholder_file = Macros.hatecheck_data_file.parent / 'template_placeholders.csv'
+    
+    @classmethod
+    def get_placeholder_values(cls) -> dict:
+        raw_data = Utils.read_sv(cls.hatecheck_placeholder_file)
+        attr = raw_data['attributes']
+        data = raw_data['lines']
+        tgt_of_interest = ['IDENTITY_S', 'IDENTITY_P', 'IDENTITY_A']
+        res = {
+            t: list()
+            for t in tgt_of_interest
+        }
+        for d in data:
+            ph_name = d[0].strip('[]')
+            if ph_name in tgt_of_interest:
+                res[ph_name] = [g.strip() for g in d[1].strip('\"\"').split(',')]
+            # end if
+        # end for
+        return res
+    
