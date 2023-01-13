39c3d89851d8b0612d72f23b9db56f54cca40f9a
jason lee <jasonlee@jasons-MacBook-Pro.local>
2023-01-13 15:27:18 -0600

    add hsd rule table

--- python/hs/paper/Tables.py
+++ python/hs/paper/Tables.py
@@ -49,8 +49,11 @@ class Tables:
             which = [which]
         # end if
 
-        for item in which: 
-            if item == "test-results":
+        for item in which:
+            if item == 'lc-req':
+                cls.make_numbers_lc_requirement(Macros.result_dir, tables_dir)
+                cls.make_table_lc_requirement(Macros.result_dir, tables_dir)
+            elif item == 'test-results':
                 task = options.pop('task', 'hs')
                 search_dataset = options.pop('search_dataset_name', 'hatexplain')
                 selection_method = options.pop('selection_method', 'random')
@@ -58,7 +61,7 @@ class Tables:
                 num_trials = options.pop('num_trials', -1)
                 cls.make_numbers_test_results(Macros.result_dir, tables_dir, task, search_dataset, selection_method, num_seeds, num_trials)
                 cls.make_table_test_results(Macros.result_dir, tables_dir, task, search_dataset, selection_method, num_seeds, num_trials)
-            elif item == "test-results-baseline":
+            elif item == 'test-results-baseline':
                 task = options.pop('task', 'hs')
                 search_dataset = options.pop('search_dataset_name', 'hatexplain')
                 selection_method = options.pop('selection_method', 'random')
@@ -70,6 +73,78 @@ class Tables:
                 raise(f"Unknown table {item}")
             # end if
         # end for
+        return
+
+    @classmethod
+    def replace_latex_symbol(cls, string):
+        for symbol in cls.LATEX_SYMBOL_MAP.keys():
+            string = string.replace(symbol, cls.LATEX_SYMBOL_MAP[symbol])
+        # end for
+        return string
+
+    @classmethod
+    def make_numbers_lc_requirement(cls, results_dir: Path, tables_dir: Path):
+        req_dir = results_dir / 'reqs'
+        req_file = req_dir / 'requirements_desc_hs.txt'
+        output_file = latex.File(tables_dir / 'lc-hsd-requirement-numbers.tex')
+        for l_i, l in enumerate(Utils.read_txt(req_file)):
+            l_split = l.strip().split('::')
+            if len(l_split)>3:
+                desc, search, exclude, transform = l_split[0], l_split[1], l_split[2], l_split[3]
+                desc = cls.replace_latex_symbol(desc)
+                search = r"\textbf{Search}~"+cls.replace_latex_symbol(search.split('<SEARCH>')[-1])
+                search += "~exlcude: "+cls.replace_latex_symbol(exclude.split('<EXCLUDE>')[-1])
+                transform = r"\textbf{Transform}~"+cls.replace_latex_symbol(transform.split('<TRANSFORM>')[-1])
+            else:
+                desc, search, exclude, transform = l_split[0], l_split[1], None, l_split[2]
+                desc = cls.replace_latex_symbol(desc)
+                search = r"\textbf{Search}~"+cls.replace_latex_symbol(search.split('<SEARCH>')[-1])
+                transform = r"\textbf{Transform}~"+cls.replace_latex_symbol(transform.split('<TRANSFORM>')[-1])
+            # end if
+            output_file.append_macro(latex.Macro(f"lc_{l_i+1}_desc", desc))
+            output_file.append_macro(latex.Macro(f"lc_{l_i+1}_search", search))
+            output_file.append_macro(latex.Macro(f"lc_{l_i+1}_transform", transform))
+        # end for
+        output_file.save()
+        return
+
+    @classmethod
+    def make_table_lc_requirement(cls, results_dir: Path, tables_dir: Path):
+        output_file = latex.File(tables_dir / "lc-hsd-requirement-table.tex")
+        
+        # Header
+        output_file.append(r"\begin{table*}[t]")
+        output_file.append(r"\begin{small}")
+        output_file.append(r"\begin{center}")
+        output_file.append(r"\caption{\HsdReqTableCaption}")
+        output_file.append(r"\begin{tabular}{p{5cm}||p{9cm}}")
+        output_file.append(r"\toprule")
+        
+        # Content
+        output_file.append(r"\tLc & \tRules \\")
+        output_file.append(r"\midrule")
+        
+        req_dir = results_dir / 'reqs'
+        req_file = req_dir / 'requirements_desc_hs.txt'
+        for l_i, l in enumerate(Utils.read_txt(req_file)):
+            l_split = l.split('::')
+            len_l = len(l_split) # 3 or 4
+            output_file.append("\multirow{3}{*}{\parbox{5cm}{" + \
+                               f"LC{l_i+1}: " + latex.Macro(f"lc_{l_i+1}_desc").use() + "}}")
+            output_file.append(" & " + latex.Macro(f"lc_{l_i+1}_search").use() + r"\\")
+            output_file.append(" & " + latex.Macro(f"lc_{l_i+1}_transform").use() + r"\\")
+            output_file.append(r"\\")
+            output_file.append(r"\hline")
+        # end for
+
+        # Footer
+        output_file.append(r"\bottomrule")
+        output_file.append(r"\end{tabular}")
+        output_file.append(r"\end{center}")
+        output_file.append(r"\end{small}")
+        output_file.append(r"\vspace{\HsdReqTableVSpace}")
+        output_file.append(r"\end{table*}")
+        output_file.save()
         return
     
     @classmethod
